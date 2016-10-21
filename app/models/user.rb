7d8b0f5c26c2181925b3dd0d37625511cc915d67
Stef Schenkelaars <stef.schenkelaars@gmail.com>
2016-10-21 10:22:59 +0200

    Fix the decryption of the gamebus keys

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,16 +6,19 @@ class User < ActiveRecord::Base
   validates :gamebus_key, presence: true
   # validates :rvs_id, uniqueness: true, allow_nil: true
 
+  # Checks if the current rvs api_key is valid
   def rvs_key_valid?
     return false unless rvs_key.present?
     rvs_connection.get('/users/current').status == 200
   end
 
+  # Removes the current rvs api_key and return true when succeeded
   def rvs_disconnect
     return false unless rvs_key.present?
     rvs_connection.delete('/api-keys/current').status == 200
   end
 
+  # After attributes rvs_email and rvs_password are set, call this method to get the rvs api_key
   def rvs_get_key
     return false unless rvs_email && rvs_password
     resp = rvs_connection.post('/api-keys', email: rvs_email, password: rvs_password)
@@ -25,6 +28,7 @@ class User < ActiveRecord::Base
     return true
   end
 
+  # The faraday connection to rvs including authorization if present
   def rvs_connection
     connection = Faraday.new url: Rails.application.secrets.rvs_url do |faraday|
       faraday.request  :url_encoded             # form-encode POST params
@@ -35,15 +39,14 @@ class User < ActiveRecord::Base
     return connection
   end
 
+  # The dynamically decrypted gamebus_key
   def gamebus_key
-    # decrypt(self[:gamebus_key])
-    # Web api call authorizaiton header:
-    # Bearer e484ef2f-e6b9-4bce-aa2d-7afefcb51d85
-    "e484ef2f-e6b9-4bce-aa2d-7afefcb51d85"
+    decrypt(self[:gamebus_key])
   end
 
+  # Faraday connection to gamebus with authorization setup
   def gamebus_connection
-    connection = Faraday.new url: 'https://gamebus.synersec.eu' do |faraday|
+    connection = Faraday.new url: Rails.application.secrets.gamebus_url do |faraday|
       faraday.request  :url_encoded                      # form-encode POST params
       faraday.response :logger if Rails.env.development? # log requests to STDOUT
       faraday.adapter  Faraday.default_adapter           # make requests with Net::HTTP
@@ -53,6 +56,8 @@ class User < ActiveRecord::Base
     return connection
   end
 
+  # Method to dynamically add items to an array with protection for StaleObjects
+  # So even if multiple processes are calling this, it will go alright
   def add_to_array attribute, value
     update! attribute => self[attribute].push(value.to_s).uniq
   rescue ActiveRecord::StaleObjectError
@@ -63,13 +68,15 @@ class User < ActiveRecord::Base
   private
 
   def decrypt(token)
-    (decipher.update(Base64.decode64(token)) + decipher.final).force_encoding('UTF-8')
+    decoded = Base64.urlsafe_decode64(token)
+    (decipher.update(decoded) + decipher.final).force_encoding('UTF-8')
   end
 
   def decipher
-    decipher = OpenSSL::Cipher::AES.new(128, :CFB)
-    # decipher.decrypt
-    decipher.key = decipher.iv = Rails.application.secrets.gamebus_app_key
+    decipher = OpenSSL::Cipher::AES.new(128, :CFB8)
+    decipher.decrypt
+    decipher.key = Rails.application.secrets.gamebus_app_key
+    decipher.iv = Rails.application.secrets.gamebus_app_key
     decipher
   end
 
