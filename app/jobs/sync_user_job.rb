513c18c84e0612c53f5632cecdbee3f9abc729e1
Stef Schenkelaars <stef.schenkelaars@gmail.com>
2017-03-13 09:48:28 +0100

    First step in pushing scores

--- app/jobs/sync_user_job.rb
+++ app/jobs/sync_user_job.rb
@@ -12,5 +12,20 @@ class SyncUserJob < ApplicationJob
     else
       raise "Invalid rvs key of user #{user.id} with gamebus id #{user.gamebus_id} and rvs id #{user.rvs_id}"
     end
+
+    # Sync the event scores
+    events_resp = user.rvs_connection.get("/users/#{user.rvs_id}/events?include[]=attendees.scores.user&parsed_intervals=2000m")
+    if events_resp.status == 200
+      JSON.parse(events_resp.body).each do |event|
+        next if user.synced_score_ids.include?(event['id'].to_s)
+        event['attendees'].each do |attendee|
+          next unless attendee['user_id'] == user.rvs_id
+          next if attendee['scores'].blank?
+          SyncScoreJob.perform_later user.id, event, attendee['scores'].first
+        end
+      end
+    else
+      raise "Invalid rvs key of user #{user.id} with gamebus id #{user.gamebus_id} and rvs id #{user.rvs_id}"
+    end
   end
 end
